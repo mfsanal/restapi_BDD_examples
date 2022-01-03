ddab809fa079d97b046aae38751ae38d6b96ec1a
Mustafa Maden <mustafa.maden@ciceksepeti.com>
2022-01-03 20:51:16 +0300

    report add

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src.test.java.features",
-        glue="steps"
-        )
+        features = "src/test/java/features",
+        glue = "steps", plugin = {"pretty", "json:target/cucumber-report.json"}
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
